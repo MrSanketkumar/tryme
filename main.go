d53f8f0a2175f16b4facb3cd8c52b0ef48a6ebd1
sanket <sanchaud@redhat.com>
2024-10-27 13:59:23 +0530

    the origin is verified

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Print("hello world")
+	fmt.Println("hello world")
 	fmt.Println("Hello world feature/bugfix")
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
 }
