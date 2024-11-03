67fabf72a5d85d37d97e27b8773828edd57bd0bd
sanket <sanchaud@redhat.com>
2024-11-03 13:54:34 +0530

    negative value is passed

--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import (
 	"tryme/sub"
 )
 
-func main()  {
-	result:=add(1,2)
-	fmt.Println("the result of the sum is:",result)
-	subResult:=sub.Sub(1,2)
-	fmt.Println("the sub is :",subResult)
+func main() {
+	result := add(10, -2)
+	fmt.Println("the result of the sum is:", result)
+	subResult := sub.Sub(1, 2)
+	fmt.Println("the sub is :", subResult)
 	fmt.Println("demo")
 }
 
